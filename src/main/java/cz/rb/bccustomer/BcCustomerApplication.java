eaca1d8fb3aab8ed49e8ae5a95ff2523e4fe7184
miraskazik <kazik.m@email.cz>
2024-12-06 20:50:37 +0100

    001 - ziskani psu a psa podle jmena z DB

---
+++ src/main/java/cz/rb/bccustomer/BcCustomerApplication.java
@@ -0,0 +1,13 @@
+package cz.rb.bccustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/rb/bccustomer/BcCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.rb.bccustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
